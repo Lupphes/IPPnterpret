50b7ec42f37a465fa974d5952775a60ada798bde
Luppo <ondrej.sloup@protonmail.com>
2021-02-23 00:10:46 +0100

    XML magic is happening

--- parser.php
+++ parser.php
@@ -35,7 +35,7 @@ function commentsTrim($line) {
 }
 
 function headerValidation($line, $header) {
-    $headerPattern = ".IPPcode21";
+    // $headerPattern = ".IPPcode21";
     if (preg_match("/^.ippcode21$/i", $line)) {
         $header = true;
     } else {
@@ -46,123 +46,183 @@ function headerValidation($line, $header) {
 }
 
 
+function generateFile($xml) {
+    file_put_contents('result.xml', $xml->outputMemory());
+}
+
 function main() {
+    // $enum = Array(
+    //     "MOVE" => "MOVE",
+    //     "CREATEFRAME" => "CREATEFRAME",
+    //     "PUSHFRAME" => "PUSHFRAME",
+    //     "POPFRAME" => "POPFRAME",
+    //     "DEFVAR" => "DEFVAR",
+    //     "CALL" => "CALL",
+    //     "RETURN" => "RETURN",
+    //     "PUSHS" => "PUSHS",
+    //     "POPS" => "POPS",
+    //     "ADD" => "ADD",
+    //     "SUB" => "SUB",
+    //     "MUL" => "MUL",
+    //     "IDIV" => "IDIV",
+    //     "LT" => "LT",
+    //     "GT" => "GT",
+    //     "EQ" => "EQ",
+    //     "AND" => "AND",
+    //     "OR" => "OR",
+    //     "NOT" => "NOT",
+    //     "INT2CHAR" => "INT2CHAR",
+    //     "STRI2INT" => "STRI2INT",
+    //     "READ" => "READ",
+    //     "WRITE" => "WRITE",
+    //     "CONCAT" => "CONCAT",
+    //     "STRLEN" => "STRLEN",
+    //     "GETCHAR" => "GETCHAR",
+    //     "SETCHAR" => "SETCHAR",
+    //     "TYPE" => "TYPE",
+    //     "LABEL" => "LABEL",
+    //     "JUMP" => "JUMP",
+    //     "JUMPIFEQ" => "JUMPIFEQ",
+    //     "JUMPIFNEQ" => "JUMPIFNEQ",
+    //     "EXIT" => "EXIT",
+    //     "DPRINT" => "DPRINT",
+    //     "BREAK" => "BREAK"
+    //     );
+
+
+
+
     $header = false;
+    $i = 0;
     argumentsValidation();
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-    
+    $xml = new XMLWriter();
+    $xml->openMemory();
+    $xml->startDocument("1.0", "UTF-8");
+
+
+
     while ($line = fgets(STDIN)) {
         $line = commentsTrim($line);
         if ($line == "") { continue; }
         if (!$header) {
             $header = headerValidation($line, $header);
+            $xml->startElement("program");
+            $xml->writeAttribute("language", "IPPcode21");
             continue;
         }
-        echo $line."\n";
-       
-       
-    
-    
-        // switch ($line) {
-        //     case "MOVE":
-        //         echo "i equals 0";
-        //         break;
-        //     case "CREATEFRAME":
-        //         echo "i equals 0";
-        //         break;
-        //     case "PUSHFRAME":
-        //         echo "i equals 0";
-        //         break;
-        //     case "POPFRAME":
-        //         echo "i equals 0";
-        //         break;
-        //     case "DEFVAR":
-        //         echo "i equals 0";
-        //         break;
-        //     case "CALL":
-        //         echo "i equals 0";
-        //         break;
-        //     case "RETURN":
-        //         echo "i equals 0";
-        //         break;
-        //     case "PUSHS":
-        //         echo "i equals 0";
-        //         break;
-        //     case "POPS":
-        //         echo "i equals 0";
-        //         break;
-        //     case "ADD":
-        //         echo "i equals 0";
-        //         break;
-        //     case "SUB":
-        //         echo "i equals 0";
-        //         break;
-        //     case "MUL":
-        //         echo "i equals 0";
-        //         break;
-        //     case "IDIV":
-        //         echo "i equals 0";
-        //         break;
-        //     case "LT":
-        //     case "GT":
-        //     case "EQ":
-        //         echo "i equals 0";
-        //         break;
-        //     case "AND":
-        //     case "OR":
-        //     case "NOT":
-        //         echo "i equals 0";
-        //         break;
-        //     case "INT2CHAR":
-        //         echo "i equals 0";
-        //         break;
-        //     case "STRI2INT":
-        //         echo "i equals 0";
-        //         break;
-        //     case "READ":
-        //         echo "i equals 0";
-        //         break;
-        //     case "WRITE":
-        //         echo "i equals 0";
-        //         break;
-        //     case "CONCAT":
-        //         echo "i equals 0";
-        //         break;
-        //     case "STRLEN":
-        //         echo "i equals 0";
-        //         break;
-        //     case "GETCHAR":
-        //         echo "i equals 0";
-        //         break;
-        //     case "SETCHAR":
-        //         echo "i equals 0";
-        //         break;
-        //     case "TYPE":
-        //         echo "i equals 0";
-        //         break;
-        //     case "LABEL":
-        //         echo "i equals 0";
-        //         break;
-        //     case "JUMP":
-        //         echo "i equals 0";
-        //         break;
-        //     case "JUMPIFEQ":
-        //         echo "i equals 0";
-        //         break;
-        //     case "JUMPIFNEQ":
-        //         echo "i equals 0";
-        //         break;
-        //     case "EXIT":
-        //         echo "i equals 0";
-        //         break;
-        //     case "DPRINT":
-        //         echo "i equals 0";
-        //         break;
-        //     case "BREAK":
-        //         echo "i equals 0";
-        //         break;
-        // }
+        // echo $line."\n";  // ignore this thats debug
+        $xml->startElement("instruction");
+        $xml->writeAttribute("order", ++$i);
+        $split = explode(' ', $line);
+        // var_dump($line);
+        $xml->writeAttribute("opcode", strtoupper($split[0]));
+
+        switch ($line) {
+            case "MOVE":
+                echo "yet to be implemented";
+                break;
+            case "CREATEFRAME":
+                echo "yet to be implemented";
+                break;
+            case "PUSHFRAME":
+                echo "yet to be implemented";
+                break;
+            case "POPFRAME":
+                echo "yet to be implemented";
+                break;
+            case "DEFVAR":
+                echo "yet to be implemented";
+                break;
+            case "CALL":
+                echo "yet to be implemented";
+                break;
+            case "RETURN":
+                echo "yet to be implemented";
+                break;
+            case "PUSHS":
+                echo "yet to be implemented";
+                break;
+            case "POPS":
+                echo "yet to be implemented";
+                break;
+            case "ADD":
+                echo "yet to be implemented";
+                break;
+            case "SUB":
+                echo "yet to be implemented";
+                break;
+            case "MUL":
+                echo "yet to be implemented";
+                break;
+            case "IDIV":
+                echo "yet to be implemented";
+                break;
+            case "LT":
+            case "GT":
+            case "EQ":
+                echo "yet to be implemented";
+                break;
+            case "AND":
+            case "OR":
+            case "NOT":
+                echo "yet to be implemented";
+                break;
+            case "INT2CHAR":
+                echo "yet to be implemented";
+                break;
+            case "STRI2INT":
+                echo "yet to be implemented";
+                break;
+            case "READ":
+                echo "yet to be implemented";
+                break;
+            case "WRITE":
+                echo "yet to be implemented";
+                break;
+            case "CONCAT":
+                echo "yet to be implemented";
+                break;
+            case "STRLEN":
+                echo "yet to be implemented";
+                break;
+            case "GETCHAR":
+                echo "yet to be implemented";
+                break;
+            case "SETCHAR":
+                echo "yet to be implemented";
+                break;
+            case "TYPE":
+                echo "yet to be implemented";
+                break;
+            case "LABEL":
+                echo "yet to be implemented";
+                break;
+            case "JUMP":
+                echo "yet to be implemented";
+                break;
+            case "JUMPIFEQ":
+                echo "yet to be implemented";
+                break;
+            case "JUMPIFNEQ":
+                echo "yet to be implemented";
+                break;
+            case "EXIT":
+                echo "yet to be implemented";
+                break;
+            case "DPRINT":
+                echo "yet to be implemented";
+                break;
+            case "BREAK":
+                echo "yet to be implemented";
+                break;
+        }
+        $xml->endElement();
     }
+    $xml->endElement();
+    $xml->endDocument();
+
+    generateFile($xml);
+
 }
 
 
